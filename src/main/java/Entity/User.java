e79db0eab59a912d7854c8c580729007c86fe24a
Dabo <zeineboudabo2003@gmail.com>
2023-12-30 22:29:02 +0100

    med

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,16 @@
+
+package Entity;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String motDePasse;
+
+   
+}
